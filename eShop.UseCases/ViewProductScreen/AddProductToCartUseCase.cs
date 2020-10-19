1375d0e267b630423011362ac21adea72505c55a
ir4van <ir4van@gmail.com>
2020-10-19 16:01:42 +0700

    add AddProductToCartUseCase

---
+++ eShop.UseCases/ViewProductScreen/AddProductToCartUseCase.cs
@@ -0,0 +1,27 @@
+﻿using eShop.UseCases.PluginInterfaces.DataStore;
+using eShop.UseCases.PluginInterfaces.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShop.UseCases.ViewProductScreen
+{
+    public class AddProductToCartUseCase
+    { 
+        private readonly IProductRepository productRepository;
+        private readonly IShopingCart shopingCart;
+
+        public AddProductToCartUseCase(IProductRepository product, IShopingCart cart)
+        {
+            this.productRepository = product;
+            this.shopingCart = cart;
+        }
+
+        public async void Execute(int productId)
+        {
+            var product = productRepository.GetProduct(productId);
+
+            await shopingCart.AddProductAsync(product);
+        }
+    }
+}
